3aa6a6a387eec77805c11fa3c317e6ccbf789b9e
c951301435@qq.com <cao951301435>
2020-05-05 21:48:22 +0800

    Initial commit

---
+++ coadmin-admin/src/test/java/com/java/项目入口ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 项目入口ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
